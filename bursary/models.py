8bd14de5f938f0ab09772b884739d1ae01f3ee4b
ChrisAchinga <achinga.chris@gmail.com>
2020-02-02 16:45:56 +0300

    created applicant

--- bursary/models.py
+++ bursary/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# model for bursary applicant
+class Applicant(models.Model):
+    first_name = models.CharField(max_length=5)
+    middle_name = models.CharField(max_length=5)
+    last_name = models.CharField(max_length=5)
+    
