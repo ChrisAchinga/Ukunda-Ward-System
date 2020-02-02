2191d6bcd759eb1d10caee5b34d70abe02b44b39
ChrisAchinga <achinga.chris@gmail.com>
2020-02-02 22:18:02 +0300

    restarted

--- bursary/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Applicant, UniversityApplicant, TechnicalCollege, HighSchool
-
-admin.site.register(Applicant)
-admin.site.register(UniversityApplicant)
-admin.site.register(TechnicalCollege)
-admin.site.register(HighSchool)
--- bursary/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BursaryConfig(AppConfig):
-    name = 'bursary'
