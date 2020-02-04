385f91dd497d3896898cd2642736798913010738
ChrisAchinga <achinga.chris@gmail.com>
2020-02-04 11:01:02 +0300

    /

---
+++ backup/bursary/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+# from .models import Applicants
+
+
+# # Creating Admin Models
+# class ApplicantAdmin(admin.ModelAdmin):
+#     list_display = ('first_name', 'last_name', 'gender')
+#     list_filter = ('first_name', 'last_name', 'gender',)
+
+
+# admin.site.register(Applicants, ApplicantAdmin)
