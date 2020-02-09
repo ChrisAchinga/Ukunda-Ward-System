7737d49fcb003d22b5d0d826154bca08bf1b8828
ChrisAchinga <achinga.chris@gmail.com>
2020-02-09 23:07:38 +0300

    02/09/2020 testing push

--- bursary/views.py
+++ bursary/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
-
+    
+# landing page views
 def landingPage(request):
     return render(request, 'bursary/index.html')
+
+# admin page view
+def adminPage(request):
+    return render(request, 'bursary/admin.html')
