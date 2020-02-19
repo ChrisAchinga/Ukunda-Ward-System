47dd44d6e0406ac553405ed3efa6599052aa9949
jumaali1449 <alijuma232@gmail.com>
2020-02-19 16:41:13 +0300

    creating routes

--- UKUNDAWARD/urls.py
+++ UKUNDAWARD/urls.py
@@ -5,5 +5,11 @@ from bursary import views as v
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', v.landingPage, name='home'),
-    path('chart/', v.chartView, name='chart')
+    path('index.html/', v.landingPage, name='home'),
+    path('chart-chartjs/', v.chartView, name='chart'),
+    path('bursary_form_component/', v.bursaryView, name='bursary'),
+    path('login/', v.loginView, name='login'),
+    path('profile/', v.profileView, name='profile'),
+    path('contact/', v.contactView, name='contact'),
+    path('404/', v.notFoundView, name='notfound')    
 ]
--- bursary/views.py
+++ bursary/views.py
@@ -10,4 +10,24 @@ def adminPage(request):
 
 # charts views
 def chartView(request):
-    return render(request, 'bursary/charts,html')
+    return render(request, 'bursary/chart-chartjs.html')
+
+#apply bursar view
+def bursaryView(request):
+    return render(request, 'bursary/bursary_form_component.html')
+
+#login view
+def loginView(request):
+    return render(request, 'bursary/login.html')
+
+#profile view 
+def profileView(request):
+    return render(request, 'bursary/profile.html')
+
+#contact view
+def contactView(request):
+    return render(request, 'bursary/contact.html')
+
+#404 view
+def notFoundView(request):
+    return render(request, 'bursary/404.html')
