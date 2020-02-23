ede791c48d24130dd7989de17c9ae397fed49797
jumakiwaka <alijuma232@gmail.com>
2020-02-24 00:58:12 +0300

    mocking chatbot,todo functionality

--- static/js/custom_ui.js
+++ static/js/custom_ui.js
@@ -1,5 +1,5 @@
 // the ui form for tertiary applicant -- appears when tertiary is selected from education level field
-function displayTertiaryApplication() {
+function toggleApplication() {
   const tertiaryForm = document.querySelector('.tertiary');
   const secondaryForm = document.querySelector('.secondary');
 
@@ -19,4 +19,68 @@ function displayTertiaryApplication() {
   }
 }
 
-displayTertiaryApplication()
+// make fake chat ui
+function sendChatMsg() {
+  const chatForm = document.querySelector('form.chat-msg');
+  const chatBox = document.querySelector('form.chat-msg input');
+  const chatBin = document.querySelector('ul.chats');
+
+  chatForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    chatBin.innerHTML += `<li class="by-other">
+
+    <div class="avatar pull-right">
+      <img src="/static/img/avatar1_small.jpg" alt="" />
+    </div>
+
+    <div class="chat-content">
+
+      <div class="chat-meta">3 hours ago <span class="pull-right">Ward Admin</span></div>
+     ${chatBox.value}
+      <div class="clearfix"></div>
+    </div>
+  </li>
+  <li class="by-me">
+
+  <div class="avatar pull-left">
+    <img src="/static/img/user.jpg" alt="" />
+  </div>
+
+  <div class="chat-content">
+
+    <div class="chat-meta">Juma Kiwaka <span class="pull-right">3 hours ago</span></div>
+    Hi!
+    <div class="clearfix"></div>
+  </div>
+</li>`;
+
+    chatBox.value = '';
+
+  })
+}
+
+function addTodo() {
+  const addTodoForm = document.querySelector('form.todo');
+  const todoDesc = document.querySelectorAll('form.todo input');
+  const tasks = document.querySelector('table.personal-task > tbody');
+
+  addTodoForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    tasks.innerHTML += `
+    <tr>
+    <td>${todoDesc[1].value.replace('/', '-')}, ${todoDesc[2].value}</td>
+    <td>
+    ${todoDesc[0].value}
+    </td>
+    <td>
+      <span class="badge bg-success">Task</span>
+    </td>
+    <td>
+      <div id="work-progress3"></div>
+    </td>
+  </tr>`;
+    console.log(todoDesc[2]);
+
+  })
+}
+
