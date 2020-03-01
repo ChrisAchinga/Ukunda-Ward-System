2642c7c6668166f03e188adab3e379461aba43c0
jumakiwaka <alijuma232@gmail.com>
2020-03-01 07:16:54 +0300

    adding year ref to urls---reducing mock data

--- UKUNDAWARD/urls.py
+++ UKUNDAWARD/urls.py
@@ -2,17 +2,19 @@ from django.contrib import admin
 from django.urls import path
 from bursary import views as v
 
+year = '2020/2021'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', v.landingPage, name='home'),
     path('index.html/', v.landingPage, name='home'),
     path('chart-chartjs/', v.chartView, name='chart'),
-    path('bursary_form_component/', v.bursaryView, name='bursary'),
-    path('applicants/', v.applicantsView, name='applicants'),
-    path('universityapplicants/', v.uni_applicantsView, name='uni_applicants'),
-    path('secondaryapplicants/', v.sec_applicantsView, name='sec_applicants'),
+    path('bursary_form_component/' + year, v.bursaryView, name='bursary'),
+    path('applicants/' + year, v.applicantsView, name='applicants'),
+    path('universityapplicants/' + year, v.uni_applicantsView, name='uni_applicants'),
+    path('secondaryapplicants/' + year, v.sec_applicantsView, name='sec_applicants'),
     path('login/', v.loginView, name='login'),
     path('profile/', v.profileView, name='profile'),
     path('contact/', v.contactView, name='contact'),
-    path('404/', v.notFoundView, name='notfound')    
+    path('404/', v.notFoundView, name='notfound'),
+    path('api/fake/addApplicant', v.updateFakeJsonDb, name='saveApplicant')    
 ]
